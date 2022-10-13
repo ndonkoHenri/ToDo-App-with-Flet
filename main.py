887fe21e53b65ff9aaa15221e1814cd61409a6d1
ndonkoHenri <robotcoder4@protonmail.com>
2022-10-13 09:01:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import time
 import flet
 from flet import (Page, Text, Row, TextField, ElevatedButton, Column, Checkbox, Ref, IconButton, FloatingActionButton,
-                  icons, UserControl, Tabs, Tab, Divider, Theme, SnackBar, AlertDialog, TextButton, colors, ProgressBar,
-                  Image)
+                  icons, UserControl, Tabs, Tab, Divider, Theme, SnackBar, AlertDialog, TextButton, colors,
+                  ProgressBar, )
 from flet.control_event import ControlEvent
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
 
 
 class ToDO(UserControl):
 
-    def __init__(self, page, snackbar_callback):
+    def __init__(self):
         super().__init__()
         self.main_container = None
         self.tasks_view = None
         self.text_field = None
         self.tabs = None
-        self.snackbar_callback = snackbar_callback
-        self.page = page
         self.delete_dialog = AlertDialog(
             modal=True,
             title=Text("Please confirm"),
@@ -30,20 +29,37 @@ class ToDO(UserControl):
             actions_alignment="end",
             on_dismiss=lambda e: print("Modal dialog dismissed!"),
         )
-        self._is_delete_item = False
-        self.page.dialog = self.delete_dialog
+
+        # the item which caused the opening of the delete-dialog box
+        self._actual_deleted_item = None
 
     def close_dialog(self, e):
+        """when the 'No' button of the delete-dialog is triggered, the dialog is just closed"""
         self.delete_dialog.open = False
-        self.page.update()
+        e.page.update()
 
     def delete_confirmed(self, e):
+        """
+        when the 'Yes' button of the delete-dialog is triggered,
+        the item is deleted and the delete-dialog is closed
+        """
+        self.delete_item_callback(self._actual_deleted_item)
         self.delete_dialog.open = False
-        self.page.update()
+        e.page.update()
+        e.page.show_snack_bar(
+            SnackBar(
+                Text("Deleted Item successfully!"),
+                action="OK!",
+                open=True
+            )
+        )
 
     def open_delete_dialog(self, item):
-        self.page.dialog.open = True
-        self.page.update()
+        """opens the delete-dialog, and keeps a reference to the item which triggered it(self._actual_deleted_item)"""
+        item.page.dialog = self.delete_dialog
+        item.page.dialog.open = True
+        self._actual_deleted_item = item
+        item.page.update()
 
     def update(self):
         """
@@ -73,11 +89,8 @@ class ToDO(UserControl):
 
         :param item: The to-do item to be deleted
         """
-        self.page.dialog.open = True
         self.tasks_view.current.controls.remove(item)
-        # It updates the UI.
-        self.page.update()
-        # self.update()
+        self.update()
 
     def submit_item(self, e: ControlEvent):
         """
@@ -91,8 +104,13 @@ class ToDO(UserControl):
             self.tasks_view.current.controls.append(item)
             self.text_field.current.value = ""
             self.text_field.current.counter_text = '0 chars'
-            self.snackbar_callback(self)
-            # It updates the UI.
+            self.page.show_snack_bar(
+                SnackBar(
+                    Text("Added New To-Do Item"),
+                    action="OK!",
+                    open=True
+                )
+            )
             self.update()
 
     def tabs_change(self, e):
@@ -126,22 +144,45 @@ class ToDO(UserControl):
                       controls=[
                           Row(
                               controls=[
-                                  TextField(ref=self.text_field, helper_text="What do you plan to do?",
-                                            hint_text="ex: learn flutter..", counter_text='0 chars',
-                                            keyboard_type="text", label="New Item", expand=True, text_size=20,
-                                            tooltip="Field for new items", prefix_icon=icons.LIST_ALT_ROUNDED,
-                                            autofocus=True, on_change=self.counter_text_change,
-                                            on_submit=self.submit_item),
-                                  FloatingActionButton(icon=icons.ADD, tooltip="add item",
-                                                       on_click=self.submit_item)]),
-                          Tabs(ref=self.tabs,
-                               tabs=[Tab(text='all', icon=icons.CHECKLIST_OUTLINED),
-                                     Tab(text='not yet done', icon=icons.CHECK),
-                                     Tab(text='done', icon=icons.DONE_ALL)],
-                               selected_index=0, on_change=self.tabs_change
-                               ),
-                          Column(ref=self.tasks_view, scroll="always", )
-                      ], spacing=24, width=600
+                                  TextField(
+                                      ref=self.text_field,
+                                      helper_text="What do you plan to do?",
+                                      hint_text="ex: learn flutter..",
+                                      counter_text='0 chars',
+                                      keyboard_type="text",
+                                      label="New Item",
+                                      expand=True,
+                                      text_size=20,
+                                      tooltip="Field for new items",
+                                      prefix_icon=icons.LIST_ALT_ROUNDED,
+                                      autofocus=True,
+                                      on_change=self.counter_text_change,
+                                      on_submit=self.submit_item
+                                  ),
+                                  FloatingActionButton(
+                                      icon=icons.ADD,
+                                      tooltip="add item",
+                                      on_click=self.submit_item
+                                  )
+                              ]
+                          ),
+                          Tabs(
+                              ref=self.tabs,
+                              tabs=[
+                                  Tab(text='all', icon=icons.CHECKLIST_OUTLINED),
+                                  Tab(text='not yet done', icon=icons.CHECK),
+                                  Tab(text='done', icon=icons.DONE_ALL)
+                              ],
+                              selected_index=0,
+                              on_change=self.tabs_change
+                          ),
+                          Column(
+                              ref=self.tasks_view,
+                              scroll="always"
+                          )
+                      ],
+                      spacing=24,
+                      width=630
                       )
 
 
@@ -153,7 +194,7 @@ class TodoItem(UserControl):
 
         :param item_text: The text that will be displayed in the item
         :param checkbox_change: This is a callback function that will be called when the value of checkbox changes
-        :param delete_callback: This is a function that will be called when the delete button of an item is pressed
+        :param delete_dialog_callback: This is a function that will be called when the delete button of an item is pressed
         """
         super().__init__()
         self.item_text = item_text
@@ -177,6 +218,13 @@ class TodoItem(UserControl):
         self.item_checkbox.current.label = self.text_field.current.value
         # It updates the UI.
         self.update()
+        e.page.show_snack_bar(
+            SnackBar(
+                Text("Updated Item successfully!"),
+                action="OK!",
+                open=True
+            )
+        )
 
     def edit_item(self, e):
         """
@@ -187,7 +235,6 @@ class TodoItem(UserControl):
         self.normal_view.current.visible = False
         self.edit_view.current.visible = True
         self.text_field.current.autofocus = True
-        # It updates the UI.
         self.update()
 
     def delete_item(self, e):
@@ -217,26 +264,48 @@ class TodoItem(UserControl):
         """
         return Column(
             controls=[
-                Row(ref=self.normal_view,
+                Row(
+                    ref=self.normal_view,
                     controls=[
-                        Checkbox(ref=self.item_checkbox, label=self.item_text, value=False,
-                                 on_change=self.item_checkbox_value_change),
-                        Row(controls=[
-                            IconButton(icon=icons.EDIT, icon_color=colors.LIGHT_GREEN_ACCENT_700,
-                                       on_click=self.edit_item,
-                                       tooltip="update item", ),
-                            IconButton(icon=icons.DELETE_FOREVER, icon_color=colors.RED_900,
-                                       tooltip="delete item",
-                                       on_click=self.delete_item, )])
-                    ], alignment="spaceBetween"
-                    ),
-                Row(ref=self.edit_view, visible=False,
-                    controls=[
-                        TextField(ref=self.text_field, value=self.item_text, tooltip="field to edit the item",
-                                  autofocus=False, label="Edit Item", expand=True,
-                                  suffix=ElevatedButton(text="Update", on_click=self.save_edit))
+                        Checkbox(
+                            ref=self.item_checkbox,
+                            label=self.item_text,
+                            value=False,
+                            on_change=self.item_checkbox_value_change),
+                        Row(
+                            controls=[
+                                IconButton(
+                                    icon=icons.EDIT,
+                                    icon_color=colors.LIGHT_GREEN_ACCENT_700,
+                                    on_click=self.edit_item,
+                                    tooltip="update item",
+                                ),
+                                IconButton(
+                                    icon=icons.DELETE_FOREVER,
+                                    icon_color=colors.RED_900,
+                                    tooltip="delete item",
+                                    on_click=self.delete_item,
+                                ),
+                            ]
+                        )
                     ],
-                    )
+                    alignment="spaceBetween"
+                ),
+                Row(
+                    ref=self.edit_view,
+                    visible=False,
+                    controls=[
+                        TextField(
+                            ref=self.text_field,
+                            value=self.item_text,
+                            tooltip="field to edit the item",
+                            autofocus=False,
+                            label="Edit Item",
+                            expand=True,
+                            on_submit=self.save_edit,
+                            suffix=ElevatedButton(text="Update", on_click=self.save_edit))
+                    ],
+                )
             ],
         )
 
@@ -251,28 +320,28 @@ def main(page: Page):
 
     # set a custom title for the page
     page.title = "myToDo App"
-    # page.splash
+
     # make the window always on top of other windows.
     page.window_always_on_top = True
+
     # set the horizontal alignment of the page to center(in the horizontal middle).
     page.horizontal_alignment = "center"
+
     # set the vertical alignment of the page to start, similar to the top of the page.
     page.vertical_alignment = "start"
+
     # add a custom font found in the assets/fonts file
     page.fonts = {"SF-simple": "/fonts/San-Francisco/SFUIDisplay-Light.ttf",
                   "SF-bold": "/fonts/San-Francisco/SFUIDisplay-Bold.ttf"}
 
     page.theme_mode = "light"
-    page.theme = Theme(font_family="SF-simple", use_material3=True, visual_density="compact", )
+    page.theme = Theme(
+        font_family="SF-simple",
+        use_material3=True,
+        visual_density="compact",
+    )
 
-    page.snack_bar = SnackBar(Text("Added New To-Do Item"), action="OK!", bgcolor=colors.BLACK87)
-
-    p_bar = ProgressBar(bar_height=3.5, visible=False)
-
-    # page.scroll= "always"
-    def snackbar_callback(instance):
-        page.snack_bar.open = True
-        page.update()
+    page.splash = ProgressBar(bar_height=3.5, visible=False)
 
     def change_bg_theme(e):
         """
@@ -288,23 +357,38 @@ def main(page: Page):
         time.sleep(1.2)
         page.update()
 
-    todo_instance = ToDO(page, snackbar_callback)  # an instance of our custom created class
-    theme_icon_button = IconButton(icons.DARK_MODE, selected_icon=icons.LIGHT_MODE, selected=False,
-                                   on_click=change_bg_theme)
+    todo = ToDO()
 
-    # add the Text widget, the Divider widget, and the ToDo widget to the page.
+    theme_icon_button = IconButton(
+        icons.DARK_MODE,
+        selected_icon=icons.LIGHT_MODE,
+        selected=False,
+        on_click=change_bg_theme, icon_size=40
+    )
+
     page.add(
-        p_bar,
-        Row(controls=[
-            Text(value="myToDo App", text_align="center", selectable=False, font_family="SF-bold", weight="bold",
-                 size=40),
-            Row([Image("/icons/icon-512.png", width=48, height=48, fit="contain"),
-                 theme_icon_button])
-        ],
-            alignment="spaceAround", ),
+        Row(
+            controls=[
+                Text(
+                    value="myToDo App",
+                    text_align="center",
+                    selectable=False,
+                    font_family="SF-bold",
+                    weight="bold",
+                    size=40
+                ),
+                theme_icon_button
+            ],
+            alignment="spaceAround",
+        ),
         Divider(),
-        todo_instance,
-        Text("Made by TheEthicalBoy", italic=True, color="blue", text_align="end", expand=True, )
+        todo,
+        Text(
+            "Made by TheEthicalBoy",
+            italic=True, color="blue",
+            text_align="end",
+            expand=True,
+        )
     )
 
 
